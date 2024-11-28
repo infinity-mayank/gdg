653d0789d380a6e1edeadeead0d1435b257dea33
Mayank Sharma <mack.jce02@gmail.com>
2024-11-28 11:48:01 +0530

    Added sass.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Mulish } from 'next/font/google'
-import './globals.css'
+import '../styles/globals.scss'
 
 const inter = Mulish({ subsets: ['latin'] })
 
